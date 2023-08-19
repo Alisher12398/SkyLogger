9e54c5552cf3054deec5eccd64bf1fd1a405ef38
AKhalykbayev <alisher.khalykbayev@gmail.com>
2023-08-19 18:30:39 +0400

    Remove SnapKit

--- SkyLogger/Extensions/UIView + Extension.swift
+++ SkyLogger/Extensions/UIView + Extension.swift
@@ -8,41 +8,25 @@
 import UIKit
 
 extension UIView {
-    
-    var safeArea: ConstraintBasicAttributesDSL {
-        
-        #if swift(>=3.2)
-        if #available(iOS 11.0, *) {
-            return self.safeAreaLayoutGuide.snp
-        }
-        return self.snp
-        #else
-        return self.snp
-        #endif
-    }
 
     func reAddSubview(_ view: UIView) {
-        view.snp.removeConstraints()
         view.removeFromSuperview()
         self.addSubview(view)
     }
     
     func reAddSubviews(_ views: [UIView]) {
         views.forEach({
-            $0.snp.removeConstraints()
             $0.removeFromSuperview()
             self.addSubview($0)
         })
     }
     
     func removeSubview(_ view: UIView) {
-        view.snp.removeConstraints()
         view.removeFromSuperview()
     }
     
     func removeSubviews(_ views: [UIView]) {
         views.forEach({
-            $0.snp.removeConstraints()
             $0.removeFromSuperview()
         })
     }
