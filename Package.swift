c85dfee9fb584d0c46a27126400f304e7a3015db
AKhalykbayev <alisher.khalykbayev@gmail.com>
2024-01-17 14:59:44 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SkyLogger",
-    platforms: [.iOS(v.12)]
+    platforms: [
+        .iOS(.v12)
+    ]
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
